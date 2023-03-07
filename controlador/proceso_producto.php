c89dfb87f349ff4fb03cf241de06c158456fad43
erickmontes464 <erickmontes464@gmail.com>
2023-03-07 18:14:05 -0500

    Segundo commit

--- controlador/proceso_producto.php
+++ controlador/proceso_producto.php
@@ -1,35 +1,49 @@
 <?php 
-    session_start();
-    include_once 'conexion.php';
-    $obj = new Conectar();
-
-    $producto = $obj-> buscarProducto($_REQUEST['cod']);
-
-    $accion = $_REQUEST['accion'];
-    if ($accion === "agregar") {
-        $foto=$_FILES['img']['name'];
-        $ruta=$_FILES['img']['tmp_name'];
-        $producto="images/".$foto;
-        $productosql="../images/".$foto;
-        copy($ruta, $productosql);
-
-        if (empty($producto)){
-                $obj->agregarProducto($_REQUEST['cod'], $_REQUEST['desc'], $_REQUEST['stock'], $_REQUEST['precioaddpro'],$producto);   
-        }else{
-            echo "<script>alert('Ya existe el producto')</script>";           
-        }
-    }else{
-        if ($accion === "agregarStock") {
-            $obj->agregarProductorepeat($_REQUEST['cod'], $_REQUEST['stock']); 
-        }
-    }
-    if(isset($_REQUEST['vaciar'])){
-        session_destroy();
-        header("location:../index.php");
-    }
-    echo "
-        <script> 
-            window.location.href = '../index.php';
-        </script>";  
-        
+   session_start();
+   include_once 'conexion.php';
+   $obj = new Conectar();
+   
+   $producto = $obj->buscarProducto($_REQUEST['cod']);
+   
+   $accion = $_REQUEST['accion'];
+   if ($accion === "agregar") {
+       $foto=$_FILES['img']['name'];
+       $ruta=$_FILES['img']['tmp_name'];
+       $tipo = $_FILES['img']['type'];
+   
+       if ($tipo === 'image/jpeg') {
+           $imagen = imagecreatefromjpeg($ruta);
+       } else if ($tipo === 'image/png') {
+           $imagen = imagecreatefrompng($ruta);
+       }
+   
+       // crear una imagen en formato WebP
+       $producto="images/".$foto.".webp";
+       $productosql="../images/".$foto.".webp";
+       imagewebp($imagen, $productosql, 80); // la calidad puede variar entre 0 y 100
+   
+       // liberar los recursos
+       imagedestroy($imagen);
+   
+       if (!empty($producto)){
+           $obj->agregarProducto($_REQUEST['cod'], $_REQUEST['desc'], $_REQUEST['stock'], $_REQUEST['precioaddpro'], $producto);   
+       } else {
+           echo "<script>alert('Ya existe el producto')</script>";           
+       }
+   } else {
+       if ($accion === "agregarStock") {
+           $obj->agregarProductorepeat($_REQUEST['cod'], $_REQUEST['stock']); 
+       }
+   }
+   
+   if (isset($_REQUEST['vaciar'])){
+       session_destroy();
+       header("location:../index.php");
+   }
+   
+   echo "
+       <script> 
+           window.location.href = '../index.php';
+       </script>";
+   
 ?>
